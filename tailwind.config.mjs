48b9a44cc49f861cb9575d015ecfdaa1e5a3381d
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-01-25 21:55:54 -0600

    DaisyUI init

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui"
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+    daisyui,
+  ],
 }
