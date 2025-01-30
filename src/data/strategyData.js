0ef69d80f486014ed1721fc97682824734dd8a91
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-01-30 08:44:35 -0600

    Refactored componnents and cleaned up files. Everything working.

---
+++ src/data/strategyData.js
@@ -0,0 +1,27 @@
+const strategyData = [
+  {
+    title: "Monthly Pastoral Fellowship",
+    description: "description",
+    icon: "fellowship" 
+  },
+  {
+    title: "Summer Pastoral Internships",
+   description: "description",
+   icon: "sun" 
+  },
+  {
+    title: "Extended Pastoral Residencies",
+   description: "description",
+   icon: "learn" 
+  },
+  {
+    title: "Periodic Training Events",
+   description: "description",
+   icon: "event" 
+  }
+]
+
+export default strategyData;
+
+
+ 
