3c8a17c2c91c9e8fd2537cc6220339dee60ca11c
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-01-30 17:21:55 -0600

    Added internship info - need to finish timeline + style

---
+++ src/data/internshipData.js
@@ -0,0 +1,112 @@
+const internshipData = {
+  features: [
+    {
+      title: "Church-Hosted",
+      description: "A host church will provide the primary context for ministry practice as the intern is immersed in the normal ministry activity (elder meetings, member visits, Sunday School, small groups, membership classes, prayer gatherings, preaching, hospitality, etc.). Each intern will have a supervising pastor-mentor.",
+      icon: "church",
+    },
+    {
+      title: "Cohort-Based",
+     description: "Interns will participate in the Sword and the Seeds Internship cohort spread throughout participating churches. The cohort will gather weekly for discussion and reflection on practical ministry topics as experienced in the local church setting. At scheduled times through the summer, the cohort will have extended training times together focused in four areas related to the TCT Southern Minnesota Vision: (1) Persuasive Preaching; (2) Essential Ecclesiology; (3) Practical Shepherding unto Compelling Culture; and (4) Small Place Ministry unto Multiplying Mission.",
+     icon: "fellowship",
+    },
+    {
+      title: "Church-Funded",
+     description: "The host church will be responsible to provide housing and a stipend of $4,800 for the twelve week internship. This position requires approximately twenty hours per week minimum. To maximize learning, interns are encouraged to participate in as local church ministry as possible.",
+     icon: "money",
+    },
+  ],
+  benefits: [
+    {
+      title: "Self-Assessment",
+      description: "Through the internship experiences, the intern will assess their own desire and preparedness for pastoral ministry in general and for ministry in normative size and small place settings in particular.",
+      icon: "church",
+    },
+    {
+      title: "Practical Training",
+     description: "The intern will receive mentoring from a supervising pastor and cohort mentor. Through guided reading, discussion, and participation in essential areas of pastoral ministry, the intern will grow in competence in skills essential to ministry.",
+     icon: "fellowship",
+    },
+    {
+      title: "External Assessment and Affirmation",
+     description: "The local church and supervising pastor will have the opportunity to assess (and Lord-willing, affirm) the intern’s gifts and calling to pastoral ministry.",
+     icon: "fellowship",
+    },
+    {
+      title: "Participation in the Life of a Healthy Church",
+     description: "The intern will enjoy the loving support of a local church which assumes the responsibility for his care and training. Through participation in the weekly rhythms of the local church, the intern will enjoy the encouragement of other believers as he explores his calling.",
+     icon: "fellowship",
+    },
+    {
+      title: "Blessing to the Local Church",
+     description: "The intern and the local church will have a mutually beneficial relationship. The intern will meet pastoral needs in the church and the church will be blessed as they exercise hospitable welcome to the intern.",
+     icon: "fellowship",
+    },
+  ],
+  timeline: [
+    {
+      title: "Week 1",
+      description: "Orientation to Small Place Ministry",
+      icon: "church",
+    },
+    {
+      title: "Week 2",
+      description: "Preaching",
+      icon: "church",
+    },
+    {
+      title: "Week 3",
+      description: "Ecclesiology",
+      icon: "church",
+    },
+    {
+      title: "Week 4",
+      description: "Ecclesiology",
+      icon: "church",
+    },
+    {
+      title: "Week 5",
+      description: "Preaching",
+      icon: "church",
+    },
+    {
+      title: "Week 6",
+      description: "Practical Shepherding",
+      icon: "church",
+    },
+    {
+      title: "Week 7",
+      description: "Practical Shepherding",
+      icon: "church",
+    },
+    {
+      title: "Week 8",
+      description: "Preaching",
+      icon: "church",
+    },
+    {
+      title: "Week 9",
+      description: "Applied Ecclesiology",
+      icon: "church",
+    },
+    {
+      title: "Week 10",
+      description: "Applied Ecclesiology",
+      icon: "church",
+    },
+    {
+      title: "Week 11",
+      description: "Cohort Retreat",
+      icon: "church",
+    },
+    {
+      title: "Week 12",
+      description: "Debrief / Goodbyes",
+      icon: "church",
+    },
+  ]
+
+}
+
+
+export default internshipData;
