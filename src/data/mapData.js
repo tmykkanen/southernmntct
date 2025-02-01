77146dcd359a8ca451bccae05fbb918ad7afe088
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-02-01 09:17:05 -0600

    Live with Map functionality

---
+++ src/data/mapData.js
@@ -0,0 +1,32 @@
+const churches = [
+  {
+    name: 'Sojourners Church',
+    coords: [43.644039556391355, -93.38859819147521],
+    pastor: 'Tyler Mykkanen',
+    email: 'hello@sojourners.church',
+    url: 'http://sojourners.church'
+  },
+  {
+    name: 'Redemption City Church',
+    coords: [44.043209548640746, -92.4932867351392],
+    pastor: 'Adam Pohlman',
+    email: 'elders@redemptionrochester.com',
+    url: 'https://www.redemptionrochester.com'
+  },
+  {
+    name: 'Northfield Community Church',
+    coords: [44.45266343070468, -93.15989457285775],
+    pastor: 'Josh Koehn',
+    email: 'hello@northfieldcc.org',
+    url: 'https://www.northfieldcc.org'
+  },
+  {
+    name: 'Gospel Joy Church',
+    coords: [44.1535996472263, -93.98025713443178],
+    pastor: 'Jordan Hecox',
+    email: 'jordanhecox@gmail.com',
+    url: 'https://www.gospeljoy.church'
+  },
+];
+
+export default churches;
