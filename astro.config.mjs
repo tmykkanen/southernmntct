242dfadbff597a5f10cd95ee070551ca7097fb5d
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-02-01 10:30:08 -0600

    Fixed missing styles and js for leaflet js

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
-
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
@@ -12,5 +10,5 @@ export default defineConfig({
   integrations: [
     tailwind({applyBaseStyles: false,}),
     icon(),
-    alpinejs({ entrypoint: '/src/entrypoint' })]
+    alpinejs({ entrypoint: '/src/entrypoint' })],
 });
